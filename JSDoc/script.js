afb340c119c34b828e5bd774b849168133c59502
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-11 19:02:45 +0100

    JSDoc

---
+++ JSDoc/script.js
@@ -0,0 +1,9 @@
+/**
+ * This function is used to greet someone on the command line
+ * 
+ * @param {string} name - this is the - name of the person that you want to greet
+ */
+
+function greet(name) {
+    console('Hallo' + name);
+}
